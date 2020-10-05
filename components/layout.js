c126531848379c3a4919a6e203bd1c3386185392
Sheila Kelley <spkelley26@gmail.com>
2020-10-05 16:39:01 -0700

    installed material ui

--- components/layout.js
+++ components/layout.js
@@ -9,6 +9,21 @@ function Layout({ children }) {
             <link rel="icon" href="/favicon.svg" />
         </Head>
 
+        <nav>
+            <Link href='/'>
+                <a className="class">Home</a>
+            </Link>
+            <Link href='about'>
+                <a>About</a>
+            </Link>
+            <Link href='/portfolio'>
+                <a>Portfolio</a>
+            </Link>
+            <Link href='/contact'>
+                <a>Contact</a>
+            </Link>
+        </nav>
+
         <main>{children}</main>
         </>
     )
